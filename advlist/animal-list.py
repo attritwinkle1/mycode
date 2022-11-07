cdfa7a70b9f1f839bcf395a46fac75b2d2b778d3
twinkle.attri@citi.com <attritwinkle1@gmail.com>
2022-11-07 21:55:41 +0000

    About complex lists

---
+++ advlist/animal-list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+   List of 3 letter animals"""
+
+def main():
+
+    # create a list called list1
+    list1 = [ "Fox", "Fly", "Ant", "Bee", "Cod", "Cat", "Dog", "Yak", "Cow", "Hen" ]
+
+    # display list1
+    print(list1)
+
+main()
+

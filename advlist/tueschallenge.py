047ed1eea9a7179c33ac2fe01d28b2fd39e31511
twinkle.attri@citi.com <attritwinkle1@gmail.com>
2022-11-08 15:30:50 +0000

    tuesdaychallenge

---
+++ advlist/tueschallenge.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+def main():
+    mylist = []
+
+    mylist.append(input("what is your first name"))
+    mylist.append(input("what is your last name"))
+    mylist.append(input("what is your favourite colour"))
+    print(mylist)
+
+main()    
+
